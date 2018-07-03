b70d86c5db372d9c5b90f080ab83dee6f5fe1aad
uekyorf <uekyorf@gmail.com>
2018-07-04 08:07:59 +0900

    change extend sending data

--- bin/server_socket.js
+++ bin/server_socket.js
@@ -1,23 +1,44 @@
 
+//全体のチャットデータ
+let chat_data = [
+  {
+    'usr': 'Vadar',
+    'msg': 'I am your father',
+    'time': '2018-07-03T02:51:01.831Z',
+  }
+];
+
 let server_socket = (server) => {
+
   const io = require('socket.io')(server);
 
-  io.on('connection', function (socket) {
+  //接続中のイベント
+  io.on('connection', (socket) => {
+    //接続時のイベント
     console.log('a user connected');
+
+    //切断時のイベント
     socket.on('disconnect', function () {
       console.log('user disconnected');
     });
 
-    socket.on('chat message', function (msg) {
-      console.log('message: ' + msg);
-      io.emit('chat message', msg);
+    //クライアントからの受信時のイベント
+    socket.on('send_to_server', (send_data) => {
+      let data = JSON.parse(send_data);
+
+      //メッセージが未入力の場合なにもしない
+      if (!data.msg) return;
+
+      //サーバ側で保持
+      chat_data.push(data);
+      //全クライアントへ送る
+      io.emit('send_to_client', JSON.stringify(data));
     });
   });
 
   server.listen(3000, function () {
     console.log('listening on *:3000');
   });
-
 };
 
 module.exports = server_socket;
--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -1,12 +1,41 @@
 const socket = io();
 
-document.getElementById('form').addEventListener('submit', () => {
-    const message = document.getElementById('m');
-    socket.emit('chat message', message.value);
-    message.value = '';
+const login = {
+    'usr': Math.random()
+}
+
+//送信
+document.getElementById('send_form').addEventListener('submit', () => {
+    const send_user = login.usr,
+        send_message = document.getElementById('send_message'),
+        send_time = new Date();
+    //未入力を送信しない
+    if (!send_message.value) return;
+
+    const send_data = JSON.stringify({
+        'usr': send_user,
+        'msg': send_message.value,
+        'time': `${send_time}`
+    });
+
+    //サーバへ送る
+    socket.emit('send_to_server', send_data);
+    //入力欄を白紙にする
+    send_message.value = '';
 });
 
-socket.on('chat message', (msg) => {
-    const messages = document.getElementById('messages');
-    messages.appendChild(document.createElement('li')).innerText = msg;
+//受信
+socket.on('send_to_client', (receive_data) => {
+    const data = JSON.parse(receive_data),
+        date = new Date(data.time);
+    const message = document.getElementById('chat_messages').appendChild(document.createElement('li'));
+    const template = `
+        <div>${data.usr}</div>
+        <div>${data.msg}</div>
+        <div>${date.getHours()}:${date.getMinutes()}</div>
+    `;
+
+    message.setAttribute('class', 'message');
+
+    message.innerHTML = template;
 });
